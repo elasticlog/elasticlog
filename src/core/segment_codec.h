44fb1294a0f43a6a7ba8d626be0bd9b474d45454
elasticlog <elasticlog01@gmail.com>
2016-11-25 03:52:44 +0000

    add decode method

--- src/core/segment_codec.h
+++ src/core/segment_codec.h
@@ -26,12 +26,25 @@ public:
     char* rbuffer = reinterpret_cast<char*>(& ((*buf)[0]));
     memcpy(rbuffer, static_cast<const void*>(MAGIC_KEY), 10);
     rbuffer += 10;
-    memcpy(rbuffer, static_cast<const void*>(total_size), 8);
+    memcpy(rbuffer, static_cast<const void*>(&total_size), 8);
     rbuffer += 8;
-    memcpy(rbuffer, static_cast<const void*>(header), 8);
+    memcpy(rbuffer, static_cast<const void*>(&header.offset), 8);
+    return true;
   }
 
-  bool Decode(const std::string* buf, SegmentHeader* header);
+  bool Decode(const std::string* buf, SegmentHeader* header) {
+    const char* rbuffer = buf->data();
+    std::string magic_key;
+    magic_key.resize(10);
+    char* magic_key_buf = reinterpret_cast<char*>(&(magic_key[0]));
+    memcpy(magic_key_buf, rbuffer, 10);
+    rbuffer += 10;
+    uint64_t total_size = 0;
+    memcpy(static_cast<void*>(&total_size), rbuffer, 8);
+    rbuffer += 8;
+    memcpy(static_cast<void*>(*header->offset), rbuffer, 8);
+    return true;
+  }
 };
 
 
