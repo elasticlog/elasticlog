0be367c32476a97a4f0e857447adaf36a6884134
elasticlog <elasticlog01@gmail.com>
2016-12-20 12:08:08 +0000

    update encode and decode

--- src/core/segment_codec.h
+++ src/core/segment_codec.h
@@ -22,14 +22,14 @@ public:
   ~SegmentCodec() {}
 
   bool Encode(const SegmentHeader& header, std::string* buf) {
-    uint64_t total_size = sizeof(uint64_t) * 2;
-    buf->resize(total_size + 10);
+    uint64_t entry_size = sizeof(uint64_t) * 2 + 10 + header.data_size;
+    buf->resize(entry_size);
     char* rbuffer = reinterpret_cast<char*>(& ((*buf)[0]));
     memcpy(rbuffer, static_cast<const void*>(MAGIC_KEY), 10);
     rbuffer += 10;
-    memcpy(rbuffer, static_cast<const void*>(&total_size), 8);
-    rbuffer += 8;
     memcpy(rbuffer, static_cast<const void*>(&header.offset), 8);
+    rbuffer += 8;
+    memcpy(rbuffer, static_cast<const void*>(&header.data_size), 8);
     return true;
   }
 
@@ -40,10 +40,9 @@ public:
     char* magic_key_buf = reinterpret_cast<char*>(&(magic_key[0]));
     memcpy(magic_key_buf, rbuffer, 10);
     rbuffer += 10;
-    uint64_t total_size = 0;
-    memcpy(static_cast<void*>(&total_size), rbuffer, 8);
-    rbuffer += 8;
     memcpy(static_cast<void*>(&header->offset), rbuffer, 8);
+    rbuffer += 8;
+    memcpy(static_cast<void*>(&header->data_size), rbuffer, 8);
     return true;
   }
 };
